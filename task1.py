fc464b17c765c1d276d4eee8ea481533fa33767c
tghebu <cherkas.va.work2@gmail.com>
2025-01-04 00:07:01 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,19 @@
+# 1st program
+print(9**0.5*5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+
+# 4th program
+x = '123.456'
+x = float(x)
+x *= 10
+x = int(x)
+x %= 10
+print(x)
+
