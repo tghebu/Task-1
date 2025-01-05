68b26502ee0781dbee0558f2df3017a222ba4ae8
tghebu <cherkas.va.work2@gmail.com>
2025-01-05 19:29:56 +0300

    Add files via upload

--- task1.py
+++ task1.py
@@ -10,10 +10,4 @@ print(2 * (2 + 2))
 print((2 * 2 + 2) == (2 * (2 + 2)))
 
 # 4th program
-x = '123.456'
-x = float(x)
-x *= 10
-x = int(x)
-x %= 10
-print(x)
-
+print(int(float('123.456') * 10) % 10)
